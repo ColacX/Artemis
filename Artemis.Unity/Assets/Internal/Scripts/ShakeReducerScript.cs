21e93fc62b10c29fcc1985280dcac1964d346928
ColacX <DanielZheng1989@gmail.com>
2017-04-04 15:57:51 +0200

    added: dancing skeleton

--- Artemis.Unity/Assets/Internal/Scripts/ShakeReducerScript.cs
+++ Artemis.Unity/Assets/Internal/Scripts/ShakeReducerScript.cs
@@ -7,8 +7,6 @@ public class ShakeReducerScript : MonoBehaviour {
 	public GameObject target;
 
 	private float slerp = 0.1f;
-	private Vector3 oldPosition;
-	private Quaternion oldRotation;
 
 	// Use this for initialization
 	void Start () {
@@ -16,15 +14,10 @@ public class ShakeReducerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//var parent = GetComponentInParent(typeof(Transform));
-		//Debug.Log("parent position: " + parent.transform.position);
-		//var newPosition = transform.position;
-
-		
 		transform.position = Vector3.Slerp(this.transform.position, target.transform.position, slerp);
 		transform.rotation = Quaternion.Slerp(this.transform.rotation, target.transform.rotation, slerp);
-
 		var tracker = target.GetComponent<DefaultTrackableEventHandler>();
+
 		if(tracker.Found)
 		{
 			Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
@@ -59,14 +52,5 @@ public class ShakeReducerScript : MonoBehaviour {
 				component.enabled = false;
 			}
 		}
-
-		Debug.Log("Found: " + tracker.Found);
-		//this.transform.position = target.transform.position;
-		//this.transform.rotation = target.transform.rotation;
-
-		//transform.localPosition = oldPosition - newPosition;
-
-		//oldPosition = newPosition;
-		//oldRotation = transform.rotation;
 	}
 }
