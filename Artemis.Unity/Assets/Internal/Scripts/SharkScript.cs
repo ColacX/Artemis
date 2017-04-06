ff0a3663a8ed207d4821a3193f72cb7633449897
ColacX <DanielZheng1989@gmail.com>
2017-04-06 16:21:10 +0200

    fixed shark animation

---
+++ Artemis.Unity/Assets/Internal/Scripts/SharkScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+using System;
+
+public class SharkScript : MonoBehaviour, ITrackableEventHandler
+{
+	public GameObject sharkPrefab;
+	public GameObject chaseTarget;
+	private GameObject instanceClone;
+
+	// Use this for initialization
+	void Start () {
+		var mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		if(mTrackableBehaviour)
+		{
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+	{
+		if(newStatus == TrackableBehaviour.Status.DETECTED ||
+				newStatus == TrackableBehaviour.Status.TRACKED ||
+				newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+		{
+			instanceClone = Instantiate(sharkPrefab);
+			instanceClone.transform.rotation = Quaternion.Euler(90, 180, 0) * transform.rotation;
+			instanceClone.transform.position = transform.position;
+			instanceClone.GetComponent<SharkChaseAttackScript>().chaseTarget = chaseTarget;
+		}
+		else
+		{
+			if(instanceClone)
+			{
+				Destroy(instanceClone);
+			}
+		}
+	}
+}
--- Artemis.Unity/Assets/JSAllAnimals/Vertebrata/Fishes/Shark/Demo/Scripts/SharkChaseAttackScript.cs
+++ Artemis.Unity/Assets/JSAllAnimals/Vertebrata/Fishes/Shark/Demo/Scripts/SharkChaseAttackScript.cs
@@ -1,34 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SharkChaseAttackScript : MonoBehaviour {
+public class SharkChaseAttackScript : MonoBehaviour
+{
 
 	SharkCharacter sharkCharacter;
 	public GameObject chaseTarget;
-	
-	void Start () {
-		sharkCharacter = GetComponent < SharkCharacter> ();
+	public float debugValue;
+
+	void Start()
+	{
+		sharkCharacter = GetComponent<SharkCharacter>();
 	}
 
-	void FixedUpdate () {
-		Vector3 targetRelPos=chaseTarget.transform.position-transform.position;
-
+	void FixedUpdate()
+	{
+		Vector3 targetRelPos = chaseTarget.transform.position - transform.position;
+		
 		float sqrDistance = targetRelPos.sqrMagnitude;
-		sharkCharacter.forwardAccerelation = Mathf.Clamp (sqrDistance * 0.1f, 0f, 2f);
-		if(sqrDistance<10f){
+		sharkCharacter.forwardAccerelation = Mathf.Clamp(sqrDistance * 0.1f, 0f, 2f);
+		debugValue = sqrDistance;
+
+		if(sqrDistance < 1f)
+		{
 			sharkCharacter.Bite();
 		}
 
-		targetRelPos.Normalize();
-		sharkCharacter.turnAccerelation = Vector3.Dot (targetRelPos,transform.right);
-		sharkCharacter.upDownAccerelation = Vector3.Dot (targetRelPos,transform.up);
+		if(sqrDistance < 0.3f)
+		{
+			sharkCharacter.forwardAccerelation = 0;
+		}
 
-		if (transform.up.y > 0f) {
-			sharkCharacter.rollAccerelation=-transform.right.y;
-		}else if(transform.right.y>0f){
-			sharkCharacter.rollAccerelation=-2f+transform.right.y;
-		}else if(transform.right.y>0f){
-			sharkCharacter.rollAccerelation=2f-transform.right.y;
+		targetRelPos.Normalize();
+		sharkCharacter.turnAccerelation = Vector3.Dot(targetRelPos, transform.right);
+		sharkCharacter.upDownAccerelation = Vector3.Dot(targetRelPos, transform.up);
+
+		if(transform.up.y > 0f)
+		{
+			sharkCharacter.rollAccerelation = -transform.right.y;
+		}
+		else if(transform.right.y > 0f)
+		{
+			sharkCharacter.rollAccerelation = -2f + transform.right.y;
+		}
+		else if(transform.right.y > 0f)
+		{
+			sharkCharacter.rollAccerelation = 2f - transform.right.y;
 		}
 	}
 }
